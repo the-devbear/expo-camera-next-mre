cf9aed9170c06edf944c0f1bf98a9c0f46b6aa9c
devbear <bjarne.haumann@gmail.com>
2024-04-06 11:02:17 +0200

    added example

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useCameraPermissions, CameraView } from "expo-camera/next";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  const [cameraViewVisible, setCameraViewVisible] = useState(false);
+  const [cameraVisible, setCameraVisible] = useState(false);
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return (
+      <View style={styles.container}>
+        <Text>No permission</Text>
+        <Button title="Request permission" onPress={requestPermission} />
+      </View>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>No permission</Text>
+        <Button title="Request permission" onPress={requestPermission} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Open camera view"
+        onPress={() => setCameraViewVisible(!cameraViewVisible)}
+      />
+      <Button
+        title="Open camera"
+        onPress={() => setCameraVisible(!cameraVisible)}
+      />
+      {cameraViewVisible && <CameraView style={styles.camera} />}
+      {cameraVisible && <Camera style={styles.camera} />}
     </View>
   );
 }
@@ -13,8 +48,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  camera: {
+    height: 300,
+    width: 200,
   },
 });
